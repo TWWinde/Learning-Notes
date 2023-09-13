991c9067d0bb4ce0afa867cf310bfcd215c0dd3e
TWWinde <st180408@stud.uni-stuttgart.de>
2023-09-13 13:21:12 +0200

    update

--- PyTorch/data_operation.py
+++ PyTorch/data_operation.py
@@ -17,3 +17,12 @@ x = torch.tensor([[1, 2, 3],
 x = x.unsqueeze(0)
 print(x.shape)  # 输出结果为 torch.Size([1, 2, 3])
 
+######################################.view()#########################################################
+'''.view() 是用于改变张量形状的方法。它可以让你将一个张量重塑成新的形状，new_tensor = tensor.view(new_shape) tensor 是原始的张量，
+new_shape 是你希望重塑成的新形状。new_shape 中的每个维度可以是一个整数值，也可以是 -1，表示该维度应根据其他维度的大小来自动推断。
+.view(-1) 多维转化为一维张量'''
+tensor = torch.randn(3, 4, 2)
+print(tensor.shape)  # 输出：torch.Size([3, 4, 2])
+new_tensor = tensor.view(6, 4)
+print(new_tensor.shape)  # 输出：torch.Size([6, 4])
+
