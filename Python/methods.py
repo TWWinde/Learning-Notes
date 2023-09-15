b0f2b106b2fdef7c8e5af6bcafb649d77e640718
TWWinde <twwinde@gmail.com>
2023-09-15 19:34:49 +0200

    Update methods.py

--- Python/methods.py
+++ Python/methods.py
@@ -6,3 +6,9 @@ my_dict = {'key1': 'value1', 'key2': 'value2'}
 value = my_dict.get('key1')
 print(value)  # 输出: 'value1'
 
+
+'''''''del 是一个Python关键字，用于删除变量、对象或对象中的属性。'''''''
+x = 10
+del x  # 删除变量x
+my_list = [1, 2, 3, 4, 5]
+del my_list[2]  # 删除索引为2的元素，列表变为 [1, 2, 4, 5]
