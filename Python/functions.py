f9ce3333fbb3d98e6803c92cf0198fa9547c9cef
TWWinde <twwinde@gmail.com>
2023-09-22 14:14:05 +0200

    Update functions.py

--- Python/functions.py
+++ Python/functions.py
@@ -1,14 +1,16 @@
-
 ########################################yield#######################################
 
 ''''
  是一个关键字，用于定义生成器函数。生成器函数是一种特殊类型的函数，可以用来迭代产生一系列的值，而不需要一次性将所有的值存储在内存中。
 当你在函数中使用 yield 关键字时，函数将变成一个生成器函数。每次调用生成器函数时，它会执行函数体中的代码，直到遇到 yield 语句。
 然后，函数会暂停执行，并将 yield 后面的值返回给调用者。生成器函数的状态会被保留，以便下次继续执行。'''''
+
+
 def my_generator():
     yield 1
     yield 2
     yield 3
+
 
 gen = my_generator()
 
@@ -44,6 +46,7 @@ print(next_element)  # 输出 "迭代器已结束"
 '''
 用于将一个文件的内容复制到另一个文件中。'''
 import shutil
+
 original_file_path = 'path/to/source/file.txt'
 new_file_path = 'path/to/destination/file.txt'
 # 复制源文件的内容到目标文件
@@ -62,11 +65,26 @@ print(result)  # 输出: True
 result = isinstance([1, 2, 3], (list, tuple))
 print(result)  # 输出: True
 
+
 # 检查一个对象是否是特定类的实例
 class MyClass:
     pass
+
 
 obj = MyClass()
 result = isinstance(obj, MyClass)
 print(result)  # 输出: True
 
+######################################## map() #######################################
+'''''
+map() 会根据提供的函数对指定序列做映射。
+第一个参数 function 以参数序列中的每一个元素调用 function 函数，返回包含每次 function 函数返回值的新列表。
+map(function, iterable, ...)'''
+
+
+def square(x):
+    return x ** 2
+
+list(map(square, [1,2,3,4,5])) # get [1, 4, 9, 16, 25]
+list(map(lambda x: x ** 2, [1, 2, 3, 4, 5]) ) # get [1, 4, 9, 16, 25]
+list(map(lambda x, y: x + y, [1, 3, 5, 7, 9], [2, 4, 6, 8, 10]))  # get [3, 7, 11, 15, 19]
