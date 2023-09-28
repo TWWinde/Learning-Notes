6acd48a8987c3a68237cc467f821a030cdbf6260
TWWinde <twwinde@gmail.com>
2023-09-28 13:51:52 +0200

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -13,5 +13,31 @@ def persistence(n):
 
     return i
 
-number = persistence(999)
+
+def pig_it(text):
+    l = 0
+    list = text.split()
+    print(list)
+    z = []
+    for x in list:
+        a = x[0]
+        n = ''
+        for i in range(len(x) - 1):
+            b = x[i + 1]
+            n += b
+        n += a
+        n += 'ay'
+        z.append(n)
+    r = ''
+    for i in range(len(z)):
+        r += z[i]
+        if i == (len(z) - 1):
+            break
+        r += ' '
+
+    return r
+
+
+number = pig_it('Pig latin is cool')
+
 print(number)
